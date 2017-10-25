7fe8624e4f4eb06a97fb5d507e9abc51e8bc3998
Igor Igeto Mitkovski <igetom@gmail.com>
2017-10-25 13:02:56 +0200

    research mode

--- sourcico-food/client/main.js
+++ sourcico-food/client/main.js
@@ -1,22 +1,13 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
 
-import './main.html';
+import App from '../imports/ui/components/App';
+import { Restaurant } from '../imports/api/restaurants';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  let restaurants = Restaurant.find({}).fetch();
+  ReactDOM.render(<App restaurants={restaurants} />, document.getElementById('app'));
 });
--- sourcico-food/server/main.js
+++ sourcico-food/server/main.js
@@ -1,5 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import { Restaurant } from '../imports/api/restaurants';
 
 Meteor.startup(() => {
+  for (let i = 0; i < 3; i++) {
+    let restaurant = new Restaurant({
+      name: `name ${i}`
+    });
+    restaurant.save();
+  }
   // code to run on server at startup
 });
